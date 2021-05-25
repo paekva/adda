83b0e55e5288fe508ff517be5796f435be50209a
paekva <paekva@yandex.ru>
2021-05-25 20:08:40 +0300

    added .gitignore and main app file

---
+++ server/src/main/java/com/itmo/adda/ADDAServerApp.kt
@@ -0,0 +1,15 @@
+package com.itmo.adda
+
+import mu.KLogging
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ADDAServerApp {
+    companion object : KLogging() {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(ADDAServerApp::class.java, *args)
+        }
+    }
+}
---
+++ server/src/test/java/ADDAServerAppRunner.kt
@@ -0,0 +1,12 @@
+package com.ifmo.adda
+
+import com.itmo.adda.ADDAServerApp
+
+class ADDAServerAppRunner {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            ADDAServerApp.main(args)
+        }
+    }
+}
