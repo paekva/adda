104856784beb3851df6241d9938085c8ff6820ef
Axis256 <32141567+Axis256@users.noreply.github.com>
2021-06-01 22:48:31 +0300

    added pagination for admin/client

--- server/src/main/java/com/ifmo/adda/service/OrdersService.kt
+++ server/src/main/java/com/ifmo/adda/service/OrdersService.kt
@@ -4,6 +4,7 @@ import com.ifmo.adda.controller.OrdersList
 import com.ifmo.adda.dao.toDto
 import com.ifmo.adda.repository.OrdersRepository
 import org.springframework.stereotype.Service
+import java.lang.Exception
 
 @Service
 class OrdersService(
@@ -13,4 +14,11 @@ class OrdersService(
     fun getOrders() = OrdersList(
         ordersRepository.findAll().map { it.toDto() }
     )
+
+    @Throws(Exception::class)
+    fun getOrdersForClient(clientId: String):OrdersList {
+        val list = ordersRepository.findForClient(clientId)
+        if (list.isEmpty()) throw Exception("Client with id $clientId not found")
+        else return OrdersList(list.map { it.toDto() })
+    }
 }
