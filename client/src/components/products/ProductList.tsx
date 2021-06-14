2d2e8cb7cc8baaafce1a6043a991dc591920d10e
paekva <paekva@yandex.ru>
2021-06-14 11:13:56 +0300

    added field for custom order

--- client/src/components/products/ProductList.tsx
+++ client/src/components/products/ProductList.tsx
@@ -5,15 +5,19 @@ import {getProductsList} from "../../api/products";
 import {ProductItem} from "./ProductItem";
 import {Product} from "../../types";
 import Button from "@material-ui/core/Button";
+import {Dialog} from "../dialog/Dialog";
 import {makeCustomOrder} from "../../api/orders";
+import {TextField} from "@material-ui/core";
 
 export type ProductListProps = {}
 const ProductList = (props: ProductListProps) => {
     const [products, setProducts] = useState<Product[]>([]);
+    const [isCustomDialog, setCustomDialog] = useState<boolean>(false);
+    const [customOrder, setCustomOrder] = useState<string>('');
     useEffect(() => {
         getProductsList()
             .then((response) => {
-                response && setProducts(response.products)
+                    response && setProducts(response.products)
                 }
             )
             .catch((e) => {
@@ -21,8 +25,39 @@ const ProductList = (props: ProductListProps) => {
             });
     }, []);
 
-    /// TODO: send description from the form filled up by the user
-    const onMakeCustomOrder = useCallback(() => makeCustomOrder('очень много печенек'), [])
+    const onMakeCustomOrder = useCallback(() => setCustomDialog(true), [])
+    const renderBody = useCallback(() => {
+        return <div style={{display: 'flex', flexDirection: 'column', padding: 10}}>
+            <TextField variant="outlined" onChange={(e) => setCustomOrder(e.target.value)}/>
+
+            <div style={{display: 'flex', flexDirection: 'row', padding: 10, justifyContent: 'space-between'}}>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="default"
+                    onClick={() => makeCustomOrder(customOrder).then(() => setCustomDialog(false))}
+                    style={{height: 56}}
+                >
+                    Создать
+                </Button>
+
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="default"
+                    onClick={() => setCustomDialog(false)}
+                    style={{height: 56}}
+                >
+                    Отмена
+                </Button>
+            </div>
+        </div>
+    }, [customOrder])
+    const renderHeader = useCallback(() => {
+        return <div>
+            Введите информацию о произвольном заказе в поле
+        </div>
+    }, [])
 
     return <div style={{display: 'flex', flexDirection: "column"}}>
         <div className='wrapper'>
@@ -40,6 +75,9 @@ const ProductList = (props: ProductListProps) => {
         >
             Создать произвольный заказ
         </Button>
+
+        {isCustomDialog &&
+        <Dialog renderBody={renderBody} renderHeader={renderHeader}/>}
     </div>
 }
 
