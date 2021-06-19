2d16a83c71ec131fd9e15f851731f9e03aefc90b
Ann <Anya-Baburova@yandex.ru>
2021-06-19 23:24:20 +0300

    Displaying the total cost of a normal order

--- client/src/components/order/OrderInfo.tsx
+++ client/src/components/order/OrderInfo.tsx
@@ -6,6 +6,7 @@ import './OrderPage.css'
 export const OrderInfo = (props: { selectedOrder?: Order | null, roles: AppRole[] }): JSX.Element => {
     const {selectedOrder, roles} = props;
     const [products, setProducts] = useState<string>('информация о заказе отсутствует')
+    const [normalOrderCost, setNormalOrderCost] = useState<string>('')
     useEffect(() => {
         if (selectedOrder && selectedOrder?.description && selectedOrder?.products?.length == 0)
             setProducts(selectedOrder?.description)
@@ -14,6 +15,11 @@ export const OrderInfo = (props: { selectedOrder?: Order | null, roles: AppRole[
                 return `${e.name}  ${e.price}`
             }).join(",  ");
             if (makeDescription) setProducts(makeDescription);
+        }
+
+        if (selectedOrder && !(selectedOrder.isCustom) && selectedOrder.products) {
+            const sum = selectedOrder.products.reduce((sum, current) => sum + parseInt(current.price), 0);
+            setNormalOrderCost(String(sum))
         }
     }, [])
     return <div className='elements'>
@@ -35,11 +41,15 @@ export const OrderInfo = (props: { selectedOrder?: Order | null, roles: AppRole[
                 </div>
 
                 <div>
-                    {selectedOrder.isCustom && !(selectedOrder.status === Status.RETURNED) && (`Стоимость: ${selectedOrder.price}`)}
+                    {selectedOrder.isCustom && !(selectedOrder.status === Status.RETURNED) && (`Стоимость: ${selectedOrder.price} (SLG)`)}
                 </div>
 
                 <div>
                     {selectedOrder.isCustom && selectedOrder.status === Status.RETURNED && (`Причина отказа: ${selectedOrder.price}`)}
+                </div>
+
+                <div>
+                    {!(selectedOrder.isCustom) && (`Общая стоимость: ${normalOrderCost} SLG`)}
                 </div>
 
             </>
